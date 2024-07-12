a1fba738bc707e2f5d01dcc0a11548330da9a396
Muhammad Aleem <m.aleem.aptech@gmail.com>
2024-07-12 17:38:36 +0500

    Admin panel added randomly

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Photography.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

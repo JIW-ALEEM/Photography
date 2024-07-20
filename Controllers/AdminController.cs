b38e551fffcaabb6dca48b56394d6a0ffcc2fba7
aptech north <aptech78@hotmail.com>
2024-07-20 14:48:42 +0500

    Check update photo problem

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 using NuGet.Protocol.Plugins;
 using Photography.Data;
 using Photography.Models;
@@ -14,19 +17,19 @@ namespace Photography.Controllers
         {
             this.db = db;
         }
-
+        [Authorize(Roles = "Admin")]
         public IActionResult AdminIndex()
         {
             return View();
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult PhotoCtg()
         {
             return View();
         }
 
-        //PhotoCategory Action Method
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public IActionResult PhotoCtg(PhotoCategory Ctg, IFormFile CategoryPhoto)
         {
@@ -56,11 +59,79 @@ namespace Photography.Controllers
             }
             return View();
         }
-
+        [Authorize(Roles = "Admin")]
         public IActionResult FetchPhotoCtg()
         {
             return View(db.PhotoCategories.ToList());
         }
 
+        [Authorize(Roles = "Admin")]
+        public IActionResult DeletePhotoCtg(int? id)
+        {
+            var data = db.PhotoCategories.FirstOrDefault(x => x.CategoryId == id);
+            db.Remove(data);
+            db.SaveChanges();
+            TempData["DelMessage"] = "Record Deleted Successfully";
+            return RedirectToAction(nameof(FetchPhotoCtg));
+        }
+
+        // Update Photo Ctg view
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult UpdatePhotoCtg(int? id)
+        {
+            var data = db.PhotoCategories.FirstOrDefault(x => x.CategoryId == id);
+            return View(data);
+        }
+
+        // Update Photo Ctg action method
+        [HttpPost]
+        public IActionResult UpdatePhotoCtg(PhotoCategory UpdatePhotoCtg1, IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                Guid r = Guid.NewGuid();
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var extensionn = file.ContentType.ToLower();
+                var exten_presize = extensionn.Substring(6);
+
+                var unique_name = fileName + r + "." + exten_presize;
+                string imagesFolder = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/img/Category");
+                string filePath = Path.Combine(imagesFolder, unique_name);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                var dbPath = Path.Combine("img/Category", unique_name);
+                UpdatePhotoCtg1.CategoryPhoto = dbPath;
+                db.Update(UpdatePhotoCtg1);
+                db.SaveChanges();
+                TempData["UpdateMessage"] = "Record Updated Successfully";
+                return RedirectToAction(nameof(FetchPhotoCtg));
+            }
+            else
+            {
+                var existingctg = db.PhotoCategories.FirstOrDefault(p => p.CategoryId == UpdatePhotoCtg1.CategoryId);
+                if (existingctg != null)
+                {
+                    UpdatePhotoCtg1.CategoryPhoto = existingctg.CategoryPhoto;
+
+                    // Detach existing tracked entity
+                    db.Entry(existingctg).State = EntityState.Detached;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Product not found";
+                    return RedirectToAction(nameof(FetchPhotoCtg));
+                }
+            }
+
+            // Update entity state
+            db.Update(UpdatePhotoCtg1);
+            db.SaveChanges();
+
+            TempData["UpdateMessage"] = file != null ? "Record Updated Successfully" : "Record Updated Successfully with Previous Image";
+            return RedirectToAction(nameof(FetchPhotoCtg));
+        }
     }
 }
